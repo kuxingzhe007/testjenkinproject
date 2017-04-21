93635c82916d042416478a1a8da2048d107c0aa4
wangkangming <wangkangmin_g@163.com>
2017-04-21 11:00:41 +0800

    first update

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+   	void Update () {
+	
+	}
+
+
+	void OnGUI(){
+		Rect rect = new Rect (0f,0f,Screen.width,Screen.height);
+		GUI.skin.label.fontSize = 40;
+		GUI.Label (rect,"hello Jenkins");
+	}
+}
